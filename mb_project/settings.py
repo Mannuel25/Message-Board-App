54114f6979a129bc04c2166811ffa43b6d7363f9
Tanimowo Emmanuel <oluwasegunprosperity@gmail.com>
2021-08-26 18:50:32 +0100

    add

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-a)65y97lj@+j_p17ebz^c*9)r5l#c^%9v4-su3&2@96@1+_ox0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['a-message-app.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
